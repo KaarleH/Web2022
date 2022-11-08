8dfc05386fff1bd89708c9786c186a712516308d
Kalle <kallehayha29@gmail.com>
2022-11-08 16:21:46 +0200

    errorit

--- week2/controllers/userController.js
+++ week2/controllers/userController.js
@@ -1,40 +1,33 @@
 'use strict';
+// userController
 const {getUser, getAllUsers, addUser} = require('../models/userModel');
 
-
-
-const user_list_get = async(req, res) => {
-    const users = await getAllUsers();
-    res.json(users);
+const user_list_get = async (req, res, next) => {
+    res.json(await getAllUsers(next));
 };
 
-const user_get = async (req, res) => {
-    const user = await getUser(req.params.id);
-    if (user.length > 0)  {
-        res.json(user.pop());
-    } else {
-        res.send("virhe");
-    }
-
+const user_get = async (req, res, next) => {
+    const user = await getUser(req.params.id, next);
+    res.json(user.pop());
 };
 
-const user_post = async (req, res) => {
-    console.log('user_post', req.body, req.file);
+const user_post = async (req, res, next) => {
+    console.log('user_post', req.body);
+
     const data = [
         req.body.name,
         req.body.email,
         req.body.passwd,
-        0,
     ];
 
-    const result = await addUser(data);
-    if(result.length > 0) {
+    const result = await addUser(data, next);
+    if (result.affectedRows > 0) {
         res.json({
             message: 'user added',
             user_id: result.insertId,
         });
     } else {
-        res.send('virhe')
+        res.send('virhe');
     }
 };
 
