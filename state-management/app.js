d8c5ecee0f3f119ba2e11843a506490784205907
KaarleH <kaarleh@metropolia.fi>
2022-11-24 10:55:37 +0200

    gps

--- state-management/app.js
+++ state-management/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const passport = require('./utils/pass');
+const passport = require('./utils/app');
 const app = express();
 const port = 3000;
 
